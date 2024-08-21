f27ac052f2c0b74127116566ece6713f6feec3f2
CarlosViniciusSantos <carlos.vinicius1@aluno.ifsp.edu.br>
2024-08-20 21:09:31 -0300

    teste Bg Longin page

--- pages/Login.js
+++ pages/Login.js
@@ -1,17 +1,36 @@
 import { StyleSheet, ImageBackground, View, Text } from 'react-native';
 
-import bg from '../assets/Group 26'
+// import bg from '../assets/Bg.svg'
 
-export default function Login(){
-    return(
-        <ImageBackground source={bg}>
-            <Text style={styles.texto}>TESTE PARA BG</Text>
-        </ImageBackground>
+export default function Login() {
+    return (
+        <View style={styles.container}>
+            <ImageBackground
+                style={styles.bg}
+                source={require('../assets/Bg.png')}>
+                <Text style={styles.texto}>TESTE PARA BG</Text>
+            </ImageBackground>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
-    texto:{
-        color:'green'
+    container:{
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor: 'white'
+    },
+    texto: {
+        color: 'green'
+    },
+    bg: {
+        justifyContent: "center",
+        alignItems: 'center',
+        width: '100%',
+        height: '100%',
+        paddingTop:90,
+        paddingBottom:100,
+        borderTopWidth:210,
+        borderTopColor:'white'
     }
 })
